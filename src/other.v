e9b76b8dc5ed77fec009246f837fac39169540d1
EmmaTheMartian <emmathemartian@gmail.com>
2024-08-28 18:39:01 -0500

    Add a quote

--- src/other.v
+++ src/other.v
@@ -6,5 +6,9 @@ pub fn other() html.HtmlElementOrString {
 	return page([
 		html.p('welcome to the other page'),
 		html.p('no, there is not anything to do here'),
+		html.hr()
+		html.p('Other: "Did you know that Turquoise is considered the best colour"')
+		html.p('Me: "I have never heard of this"')
+		html.p('Other: "It\'s ${html.em('cyantifically')} proven"')
 	])
 }
